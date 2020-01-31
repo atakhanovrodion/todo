bfe0133b1b1c9a5ea03a3f42c6d658876510fa5e
atakhanovrodion <atakhanovrodion@gmail.com>
2020-01-31 15:40:42 +0500

    0.1

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -8,7 +8,6 @@ export default class List extends React.Component {
         {list.map(item => (
           <li key={item.id}>
             {item.text}
-            <button>edit</button>
             <button onClick={() => this.props.deleteItem(item.id)}>complete</button>
           </li>
         ))}
